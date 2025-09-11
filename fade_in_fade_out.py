37cdd01c9173c6fd976fd26c1cacfd1b667e1af3
TomasThompson25 <123387246+TomasThompson25@users.noreply.github.com>
2025-09-11 11:49:40 -0500

    Update fade_in_fade_out.py

--- fade_in_fade_out.py
+++ fade_in_fade_out.py
@@ -3,5 +3,17 @@ from time import sleep
 
 
 # SETUP
+led = PWM(Pin(15))
+led.freq(1000) 
 
-# LOOP
+# LOOP 
+
+while True: 
+
+    for duty in range(0,65535, 500): 
+        led.duty_u16(duty) 
+        sleep(2/ (65535/500)) 
+       
+    for duty in range(65535, -1, -500): 
+        led.duty_u16(duty) 
+        time.sleep(1 / (65535/500))
